39ca1df962a9ee75f8c579287d7bdafc92ca3252
Dr-Banerjee <raieanmathphd@gmail.com>
2025-02-23 17:08:04 +0100

    config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
-git = "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"
-git_1 = "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"
-git_2 = "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"
+git = ""
+git_1 = ""
+git_2 = ""
+# Add your own github tokens.
